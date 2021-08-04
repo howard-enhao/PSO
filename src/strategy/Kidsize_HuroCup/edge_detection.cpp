576217ba95664ef7d6a082769fb14b13d4c14cda
yiching_T460 <a4860e@gmail.com>
2021-08-04 12:34:25 +0800

    add print edge_image type

--- src/strategy/Kidsize_HuroCup/edge_detection.cpp
+++ src/strategy/Kidsize_HuroCup/edge_detection.cpp
@@ -15,6 +15,28 @@ void Edge_detection::Catch_image(const sensor_msgs::ImageConstPtr& msg)
     }
 }
 
+string type2str(int type) {
+  string r;
+
+  uchar depth = type & CV_MAT_DEPTH_MASK;
+  uchar chans = 1 + (type >> CV_CN_SHIFT);
+
+  switch ( depth ) {
+    case CV_8U:  r = "8U"; break;
+    case CV_8S:  r = "8S"; break;
+    case CV_16U: r = "16U"; break;
+    case CV_16S: r = "16S"; break;
+    case CV_32S: r = "32S"; break;
+    case CV_32F: r = "32F"; break;
+    case CV_64F: r = "64F"; break;
+    default:     r = "User"; break;
+  }
+
+  r += "C";
+  r += (chans+'0');
+
+  return r;
+}
 
 int main(int argc, char** argv)
 {
@@ -45,7 +67,20 @@ void Edge_detection::strategymain()
         cvtColor(edge, frame, cv::COLOR_GRAY2BGR);
         edgeimage_msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
         edgeimage_Publisher.publish(edgeimage_msg);
-        
+        ROS_INFO("width %d , height %d", edge.rows, edge.cols);
+
+        string ty =  type2str( edge.type() );
+        printf("Matrix: %s %dx%d \n", ty.c_str(), edge.cols, edge.rows );
+        // for(int i = 0; i<edge.cols; i++)
+        // {
+        //     for(int j = 0; j<edge.rows; j++)
+        //     {
+        //         printf("%*u ", frame.at<uchar>(i,j));
+        //     }
+        //     printf("\n");
+        // }
+        // printf("\n\n\n");
+        for(int i = 0;i<100000000; i++);
     }
     checkImageSource = false;
 }
